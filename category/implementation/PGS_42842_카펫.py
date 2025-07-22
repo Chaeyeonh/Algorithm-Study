db2650f0099ea4ebb48c7aa7a96954c0fb9bbfb0
chaeyeonni <126552675+intelsally@users.noreply.github.com>
2025-07-22 19:14:47 +0900

    ♻️refactor/250722/카펫/42842/파이썬 풀이

--- category/implementation/PGS_42842_카펫.py
+++ category/implementation/PGS_42842_카펫.py
@@ -1,13 +1,10 @@
 def solution(brown, yellow):
-    answer = []
+
     total = brown + yellow
-    for row in range (3, total+1):
+    for row in range (3, int(total**1/2) + 1):
         if total % row == 0:
             col = total // row
             if brown == 2 * (row + col - 2):
-                answer.append(row)
-                answer.append(col)
                 break
 
-    answer.sort(reverse = True)
-    return answer
+    return [col,row]
