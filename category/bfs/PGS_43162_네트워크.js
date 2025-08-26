65dac615893b5262d56d89fb808013e7fc05091c
Chaeyeonh <strawberry@khu.ac.kr>
2025-08-26 16:04:30 +0900

    ✨feat/250826/네트워크/43162/BFS_자바스크립트풀이

---
+++ category/bfs/PGS_43162_네트워크.js
@@ -0,0 +1,29 @@
+//네트워크 bfs풀이
+function solution(n, computers){
+    const visited = Array(n).fill(false);
+    let answer = 0;
+
+    const bfs = (start) => {
+        const queue = [start];
+        visited[start] = true;
+        while(queue.length > 0){
+            const v = queue.shift();
+            for (let u = 0; u < n; u++){
+                if (computers[u][v] == 1 && !visited[u]){
+                    visited[u] = true;
+                    queue.push(u);
+                }
+            }
+        }
+    }
+
+    for (let i = 0; i < n; i++){
+        if (!visited(i)){
+            bfs(i);
+            answer ++;
+        }
+    }
+
+    return answer;
+
+}
